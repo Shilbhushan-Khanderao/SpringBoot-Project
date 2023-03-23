4cd18c68ec1acdafdd2296633568c926bc7cd2cb
Shilbhushan-Khanderao <kshilbhushan@gmail.com>
2023-03-23 18:33:59 +0530

    Started with index

---
+++ src/main/java/com/fleetms/project/ProjectApplication.java
@@ -0,0 +1,16 @@
+package com.fleetms.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
